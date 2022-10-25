11ecb06b312f5244497956a59908c31b995d3d6c
hyunpaper <hyunpaper5170@naver.com>
2022-10-25 10:44:02 +0900

    swea

--- SWEA/d2/d2_1974.cpp
+++ SWEA/d2/d2_1974.cpp
@@ -4,7 +4,14 @@ int puz[9][9];
 
 int check()
 {
-
+    for (int i = 0; i < 9; i++)
+    {
+        int check1 = 0;
+        for (int j = 0; j <9; j++)
+        {
+            int check1 += map[i][j];
+        }
+    }
     
 }
 
